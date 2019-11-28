815dac524240c81f7ab259fb2fc5bee175b07486
Mizanur Rahman <mizanur.rahman@smartwebsource.com>
2019-11-28 12:37:26 +0600

    Fixed request validator namespace issue in controller

--- app/Services/ControllerMakerService.php
+++ app/Services/ControllerMakerService.php
@@ -10,6 +10,7 @@ class ControllerMakerService
     protected $outputDirectory;
     protected $template;
     protected $configurations;
+    protected $namespace;
 
     public function __construct($config)
     {
@@ -17,6 +18,8 @@ class ControllerMakerService
         $this->configurations = $config["configurations"];
 
         $this->template = file_get_contents(public_path("templates/controller.text"));
+
+        $this->namespace = "App\Http\Requests";
     }
 
     public function make()
@@ -32,6 +35,22 @@ class ControllerMakerService
             "[MODEL_VARIABLE_NAME_SINGULAR]" => $this->configurations["model"]["variable_singular"],
             "[MODEL_VARIABLE_NAME_PLURAL]" => $this->configurations["model"]["variable_plural"],
         ];
+
+        if (!empty($this->configurations['validator']['save']['directory'])) {
+            $dirName = ucfirst($this->configurations['validator']['save']['directory']);
+            $namespace = "{$this->namespace}\\{$dirName}";
+        }else{
+            $namespace = "{$this->namespace}";
+        }
+        $payload["[SAVE_VALIDATOR_NAMESPACE]"] = "{$namespace}\\{$this->configurations['validator']['save']['name']}";
+
+        if (!empty($this->configurations['validator']['update']['directory'])) {
+            $dirName = ucfirst($this->configurations['validator']['update']['directory']);
+            $namespace = "{$this->namespace}\\{$dirName}";
+        }else{
+            $namespace = "{$this->namespace}";
+        }
+        $payload["[UPDATE_VALIDATOR_NAMESPACE]"] = "{$namespace}\\{$this->configurations['validator']['update']['name']}";
 
         $search = array_keys($payload);
         $replace = array_values($payload);
